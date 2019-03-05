e1b41bbbeb6aedf11edbe6bb6684afeee3554318
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2019-03-04 23:19:49 -0300

    Update webpack.mix.js

--- src/stubs/webpack.mix.js
+++ src/stubs/webpack.mix.js
@@ -1,7 +1,12 @@
-let mix = require('laravel-mix');
-require('laravel-mix-tailwind');
+const mix = require('laravel-mix')
+const tailwindcss = require('tailwindcss')
 
 mix
-    .js('resources/assets/js/app.js', 'js')
-    .sass('resources/assets/sass/app.scss', 'css')
-    .tailwind();
+  .js('resources/assets/js/app.js', 'js')
+  .sass('resources/assets/sass/app.scss', 'css')
+  .options({
+    processCssUrls: false,
+    postCss: [tailwindcss('./tailwind.js')]
+  })
+  .extract(['vue', 'axios'])
+  .version()
