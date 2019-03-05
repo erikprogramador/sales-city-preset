e31ea329a60efea9e67fd052ed475d7121e82ae4
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2019-03-04 23:19:09 -0300

    Add .vue files auto import

--- src/stubs/app.js
+++ src/stubs/app.js
@@ -1,7 +1,15 @@
-import './bootstrap';
-import Vue from 'vue';
-import ExampleComponent from './components/ExampleComponent';
+import Vue from 'vue'
+import './bootstrap'
 
-Vue.component('example-component', ExampleComponent);
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key =>
+  Vue.component(
+    key
+      .split('/')
+      .pop()
+      .split('.')[0],
+    files(key).default
+  )
+)
 
-const app = new Vue({}).$mount('#app');
+const app = new Vue({}).$mount('#app')
